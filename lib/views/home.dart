502f46b391263fbd9b9b1fd647d70b945ee8b01f
Martin Jablecnik <martin.jablecnik@email.cz>
2021-01-12 22:45:27 +0100

    refactor: move TabNavigationItem class into HomeView

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,7 +3,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:timetracker/controllers/home.dart';
-import 'package:timetracker/data/models/tab_navigation.dart';
+import 'package:timetracker/views/history.dart';
+import 'package:timetracker/views/review.dart';
+import 'package:timetracker/views/trackers.dart';
+
+
+class TabNavigationItem {
+  final Widget page;
+  final String title;
+  final Icon icon;
+
+  TabNavigationItem({
+    @required this.page,
+    @required this.title,
+    @required this.icon,
+  });
+
+  static List<TabNavigationItem> get items => [
+    TabNavigationItem(
+      page: TrackersView(),
+      icon: Icon(Icons.timelapse),
+      title: "Time trackers",
+    ),
+    TabNavigationItem(
+      page: ReviewView(),
+      icon: Icon(Icons.list_alt),
+      title: "Work review",
+    ),
+    TabNavigationItem(
+      page: HistoryView(),
+      icon: Icon(Icons.history),
+      title: "History",
+    ),
+  ];
+}
+
 
 class HomePage extends StatelessWidget {
   @override
