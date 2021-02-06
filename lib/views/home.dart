646ee67a40802f9cd7677447e250f7dcdf1f4e11
Martin Jablecnik <martin.jablecnik@email.cz>
2021-02-06 12:10:20 +0100

    refactor: move TrackerButton into AppBar actions

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:timetracker/controllers/home.dart';
+import 'package:timetracker/controllers/tracker_list.dart';
+import 'package:timetracker/views/dialog.dart';
 import 'package:timetracker/views/history.dart';
 import 'package:timetracker/views/review.dart';
 import 'package:timetracker/views/tracker_list.dart';
@@ -20,22 +22,22 @@ class TabNavigationItem {
   });
 
   static List<TabNavigationItem> get items => [
-    TabNavigationItem(
-      page: TrackerListView(),
-      icon: Icon(Icons.timelapse),
-      title: "Time trackers",
-    ),
-    TabNavigationItem(
-      page: ReviewView(),
-      icon: Icon(Icons.list_alt),
-      title: "Work review",
-    ),
-    TabNavigationItem(
-      page: HistoryView(),
-      icon: Icon(Icons.history),
-      title: "History",
-    ),
-  ];
+        TabNavigationItem(
+          page: TrackerListView(),
+          icon: Icon(Icons.timelapse),
+          title: "Time trackers",
+        ),
+        TabNavigationItem(
+          page: ReviewView(),
+          icon: Icon(Icons.list_alt),
+          title: "Work review",
+        ),
+        TabNavigationItem(
+          page: HistoryView(),
+          icon: Icon(Icons.history),
+          title: "History",
+        ),
+      ];
 }
 
 class HomePage extends StatelessWidget {
@@ -44,13 +46,18 @@ class HomePage extends StatelessWidget {
     return GetBuilder<HomeController>(
       init: HomeController(),
       builder: (controller) => Scaffold(
-        appBar: AppBar(title: Text("TimeTracker")),
+        appBar: AppBar(
+          title: Text("TimeTracker"),
+          actions: [
+            if (controller.index == 0)
+              NewTrackerButton()
+          ],
+        ),
         body: AnimatedIndexedStack(
           transitionBuilder: slideTransition,
           selectedIndex: controller.index,
           children: [
-            for (final tabItem in TabNavigationItem.items)
-              tabItem.page,
+            for (final tabItem in TabNavigationItem.items) tabItem.page,
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -69,12 +76,35 @@ class HomePage extends StatelessWidget {
   }
 }
 
+class NewTrackerButton extends GetView<TrackerListController> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GestureDetector(
+        child: Icon(Icons.add),
+        onTap: () => {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return TrackerDialog(
+                title: "Add new tracker",
+                onSubmit: (item) => controller.addItem(item),
+              );
+            },
+          )
+        },
+      ),
+    );
+  }
+}
+
 final RouteTransitionsBuilder slideTransition = (
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-    ) {
+  BuildContext context,
+  Animation<double> animation,
+  Animation<double> secondaryAnimation,
+  Widget child,
+) {
   return GetBuilder<HomeController>(
     init: HomeController(),
     builder: (c) {
